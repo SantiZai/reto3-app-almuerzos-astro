bc64cc59f2eda8b92f5dc5aa0181113e0dde8199
SantiZai <santyagozaidan@gmail.com>
2024-04-19 17:44:38 -0300

    manage all the info for the user

--- src/utils/store.ts
+++ src/utils/store.ts
@@ -1,3 +1,8 @@
 import { atom } from "nanostores";
 
-export const user = atom({ fullname: "", identifier: "" });
+export const user = atom({
+  id: "",
+  fullname: "",
+  identifier: "",
+  position: "",
+});
