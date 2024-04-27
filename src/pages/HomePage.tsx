de14874b278b0e7744571bef7447007ff7d1de63
SantiZai <santyagozaidan@gmail.com>
2024-04-26 21:26:29 -0300

    navigate to login page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,13 @@
+import { useNavigate } from "react-router-dom";
+
 const HomePage = () => {
+  const navigate = useNavigate();
+
   return (
     <main>
       <h1>Bienvenido</h1>
       <h3>Para crear su pedido debe iniciar sesión.</h3>
-      <button>Iniciar sesión</button>
+      <button onClick={() => navigate("login")}>Iniciar sesión</button>
     </main>
   );
 };
