375611b0f1d85903aa50e7727cc162e1b8d2a68c
SantiZai <santyagozaidan@gmail.com>
2024-04-26 21:30:38 -0300

    adding tailwind css

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,10 +1,10 @@
 const LoginPage = () => {
   return (
     <main>
-      <h1>Ingresar a mi cuenta</h1>
-      <form action="submit">
-        <input type="text" />
-        <input type="text" />
+      <h1 className="text-3xl">Ingresar a mi cuenta</h1>
+      <form action="submit" className="flex flex-col gap-4">
+        <input type="text" className="w-3/4 mx-auto" />
+        <input type="text" className="w-3/4 mx-auto" />
       </form>
     </main>
   );
