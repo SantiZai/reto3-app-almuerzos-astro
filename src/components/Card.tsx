f646c1234c74559bb639dc0eec798cb35f474721
SantiZai <santyagozaidan@gmail.com>
2024-04-28 00:24:33 -0300

    click in the menu to set this in the state

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,8 +1,17 @@
 import { Menu } from "../utils/models";
 
-const Card = ({ menu }: { menu: Menu }) => {
+const Card = ({
+  menu,
+  handleClick,
+}: {
+  menu: Menu;
+  handleClick: () => void;
+}) => {
   return (
-    <li className="p-4 my-2 flex justify-between items-center text-wrap text-left bg-slate-700 rounded-lg">
+    <li
+      onClick={handleClick}
+      className="p-4 my-2 flex justify-between items-center text-wrap text-left bg-slate-700 rounded-lg"
+    >
       <span className="w-3/4 text-balance">{menu.name}</span>
       <button>Elegir</button>
     </li>
