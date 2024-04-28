dda6a7410533949a2e57c4087b60082bdc0b42c0
SantiZai <santyagozaidan@gmail.com>
2024-04-28 00:06:56 -0300

    all the models

---
+++ src/utils/models.ts
@@ -0,0 +1,22 @@
+export interface User {
+  id: string;
+  fullname: string;
+  identifier: string;
+  position: string;
+}
+
+export interface Menu {
+  id: string;
+  name: string;
+  position: string;
+  type: string;
+}
+
+export interface Order {
+  employeeid: string;
+  menus: {
+    entradaId: string;
+    principalId: string;
+    postreId: string;
+  };
+}
