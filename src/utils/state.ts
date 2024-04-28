83ef4f9b81671db417bc615eb81198b78dcb72de
SantiZai <santyagozaidan@gmail.com>
2024-04-27 23:47:03 -0300

    set user

--- src/utils/state.ts
+++ src/utils/state.ts
@@ -1,11 +1,26 @@
 import { create } from "zustand";
 
-export const userStore = create((set) => ({
+type State = {
+  user: User;
+};
+
+type Action = {
+  setUser: (user: State["user"]) => void;
+};
+
+interface User {
+  id: string;
+  fullname: string;
+  identifier: string;
+  position: string;
+}
+
+export const userStore = create<State & Action>((set) => ({
   user: {
     id: "",
     fullname: "",
     identifier: "",
     position: "",
-  },
-  setUser: () => set((state: any) => ({ user: state.user })),
+  } as User,
+  setUser: (user: User) => set(() => ({ user })),
 }));
